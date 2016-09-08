3113071e659ab42d3f15dbcaa382db24e7d72978
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-09-08 20:01:54 +0900

    auth reducer

--- src/scripts/reducers/auth.js
+++ src/scripts/reducers/auth.js
@@ -1,25 +1,28 @@
-import { LOGGED_IN ,LOGGED_OUT } from '../actions/login';
+import {LOGGED_IN, LOGGED_OUT} from '../actions/login';
 
-const auth = ( state = {
+const auth = (state = {
 	username: '',
 	password: '',
-	loggedIn: false
-}, action ) => {
+	loggedIn: false,
+	error: false
+}, action) => {
 	switch (action.type) {
 
 		case LOGGED_IN :
-			let { username, password, loggedIn } = action;
+			let {username, password, loggedIn, error} = action;
 			return {
 				username,
 				password,
-				loggedIn
+				loggedIn,
+				error
 			}
 
 		case LOGGED_OUT :
 			return {
 				username: '',
 				password: '',
-				loggedIn: false
+				loggedIn: false,
+				error: false
 			}
 		default:
 			return state
